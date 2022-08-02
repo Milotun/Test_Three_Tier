7ccaa03ba0ca578db572122ab74f25af94194ce7
Milotun <98788545+Milotun@users.noreply.github.com>
2022-08-02 07:21:02 -0700

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-# to store statefile in s3
-terraform {
-  backend "s3" {
-    bucket = "terraform2218"
-    key    = "Hello-devops.tfstate"
-    region = "us-west-2"
-    dynamodb_table = "terraform-statefiletable"
-  }
-}
